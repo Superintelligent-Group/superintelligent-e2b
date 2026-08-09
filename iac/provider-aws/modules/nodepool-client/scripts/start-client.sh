d2c4adadc2cfc6314ddde38a07576592de41dc65
Alif Jakir <cubelocked@gmail.com>
2026-08-09 13:10:59 -0400

    fix(e2b): configure consul dns port for systemd resolved

--- iac/provider-aws/modules/nodepool-client/scripts/start-client.sh
+++ iac/provider-aws/modules/nodepool-client/scripts/start-client.sh
@@ -114,7 +114,7 @@ mkdir -p /etc/systemd/resolved.conf.d/
 touch /etc/systemd/resolved.conf.d/consul.conf
 cat <<EOF >/etc/systemd/resolved.conf.d/consul.conf
 [Resolve]
-DNS=127.0.0.1:8600
+DNS=127.0.0.1#8600
 DNSSEC=false
 DNSStubListener=yes
 DNSStubListenerExtra=172.17.0.1
